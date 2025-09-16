f2413b2fb9c5133ba2f4ebe700c81e64b58a6bae
DBCKS97 <wonyu1997@naver.com>
2025-09-16 10:42:18 +0900

    Malrancow
    eat

--- main.cpp
+++ main.cpp
@@ -4,9 +4,20 @@ using namespace std;
 
 int main()
 {
+    int Temp = 0;
     
+    // 의도가 분명하게 작성
+    Temp = (10 + 10);
+    Temp = Temp - (10 * 10) / 10 % 10;
+       
+    int DefaultMalrangCowCount = 30;
+    int TodayEatCount = 1;
+    int CurrentMalrangCowCount = DefaultMalrangCowCount - TodayEatCount;
 
-
+    cout << "Default MalrangCow  Count: " << DefaultMalrangCowCount << endl;
+    cout << "Today Eat Count: " << TodayEatCount << endl;
+    cout << "Current MalrangCow Count: " << CurrentMalrangCowCount;
+    
     return 0;
 }
 
